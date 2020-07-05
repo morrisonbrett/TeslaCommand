d1ffe404b13fd70ee53e1dbe9aa2bee3ca06dffc
zephyrus9MA <67062520+zephyrus9MA@users.noreply.github.com>
2020-07-04 20:52:09 -0700

    Add files via upload

--- lib/NotifyLib/Mail.go
+++ lib/NotifyLib/Mail.go
@@ -1,4 +1,5 @@
 package NotifyLib
+// Add second to address. Most mail servers require a FROM: address. This was added to the message by Lee Elson on 6/29/20
 
 import (
 	"fmt"
@@ -8,7 +9,7 @@ import (
 )
 
 // SendMail ...
-func SendMail(logger *log.Logger, mailServer string, mailServerPort int, mailServerLogin string, mailServerPassword string, fromAddress string, toAddress string, subj string, body string) error {
+func SendMail(logger *log.Logger, mailServer string, mailServerPort int, mailServerLogin string, mailServerPassword string, fromAddress string, toAddress1 string, toAddress2 string, subj string, body string) error {
 	// Set up authentication information.
 	var auth smtp.Auth
 	if len(mailServerLogin) > 0 {
@@ -16,10 +17,10 @@ func SendMail(logger *log.Logger, mailServer string, mailServerPort int, mailSer
 	}
 
 	// Connect to the server, authenticate, set the sender and recipient, and send the email in one step.
-	to := []string{toAddress}
-	msg := []byte("To: " + toAddress + "\r\nSubject: " + subj + "\r\n\r\n" + body + "\r\n")
+	//LSE set up second To email address
+	to := []string{toAddress1, toAddress2}
+	msg := []byte("To: " + toAddress1 + "\r\nFrom: " + fromAddress + "\r\nSubject: " + subj + "\r\n" + body + "\r\n")
 	serverPort := mailServer + ":" + strconv.Itoa(mailServerPort)
-	logger.Printf("Sending mail via server %s\n", serverPort)
 	err := smtp.SendMail(serverPort, auth, fromAddress, to, msg)
 	if err != nil {
 		return fmt.Errorf("sendMail error: %s", err)
